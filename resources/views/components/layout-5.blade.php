6e9eb6d3de2a6dcbfcd353926ba005dd67a26e81
Daniel-Ababio <danielababio279@gmail.com>
2023-05-19 20:24:14 -0700

    changes

--- resources/views/components/layout-5.blade.php
+++ resources/views/components/layout-5.blade.php
@@ -10,8 +10,10 @@
         <link rel="stylesheet" href="{{ asset('css/main.css') }}" /> --}}
         <title>BackGate</title>
         @vite('resources/js/admin-5.js')
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <script src="https://kit.fontawesome.com/c2911067ff.js" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}"> --}}
+        <script src="{{ asset('js/admin-5.js') }}" defer></script>
     </head>
     <style>
         .scroll-to-top {
