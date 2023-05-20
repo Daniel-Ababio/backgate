df9d7867edc59386d65395e01eac14a0606226af
Daniel-Ababio <danielababio279@gmail.com>
2023-05-19 20:31:46 -0700

    changes

---
+++ public/build/assets/app-layout-47c6207f.js
@@ -0,0 +1,1 @@
+import"./all.min-cc5a376f.js";/* empty css             */import"./bootstrap.bundle.min-6536ab69.js";
--- resources/views/components/layout-5.blade.php
+++ resources/views/components/layout-5.blade.php
@@ -9,7 +9,7 @@
         <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}" />
         <link rel="stylesheet" href="{{ asset('css/main.css') }}" /> --}}
         <title>BackGate</title>
-        @vite('resources/js/admin-5.js')
+        {{-- @vite('resources/js/admin-5.js') --}}
         {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <script src="https://kit.fontawesome.com/c2911067ff.js" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="{{ asset('css/app.css') }}"> --}}
