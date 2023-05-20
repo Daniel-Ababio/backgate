ba62641c62f3c4158ee7aef3d8eba74c769f8043
Daniel-Ababio <danielababio279@gmail.com>
2023-05-19 20:13:51 -0700

    changes

--- resources/views/components/layout-5.blade.php
+++ resources/views/components/layout-5.blade.php
@@ -10,6 +10,8 @@
         <link rel="stylesheet" href="{{ asset('css/main.css') }}" /> --}}
         <title>BackGate</title>
         @vite('resources/js/admin-5.js')
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <script src="https://kit.fontawesome.com/c2911067ff.js" crossorigin="anonymous"></script>
     </head>
     <style>
         .scroll-to-top {
@@ -219,6 +221,7 @@
               <i class="fas fa-angle-up text-white"></i>
             </a>
           </section>
+          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
         {{-- <script src="{{ asset('assets/js/all.min.js') }}"></script>
         <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script> --}}
