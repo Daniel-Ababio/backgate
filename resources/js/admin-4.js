a73a67b207ed6b4912a3ba708cbdc1b4f5a9ea15
Daniel-Ababio <danielababio279@gmail.com>
2023-05-21 02:00:45 -0700

    changes

--- resources/js/admin-4.js
+++ resources/js/admin-4.js
@@ -2,7 +2,7 @@ import "../assets/css/all.min.css"; //
 import "../assets/css/bootstrap.min.css"; //
 import "../assets2/css/sb-admin-2.min.css"; //
 import "../assets2/vendor/datatables/dataTables.bootstrap4.min.css"; //
-import "../assets/main.css";
+import "../assets/main.css"; //
 
 import "../assets/js/all.min.js"; //
 import "../assets2/vendor/jquery/jquery.min.js"; //
@@ -11,4 +11,4 @@ import "../assets2/vendor/jquery-easing/jquery.easing.min.js"; //
 import "../assets2/js/sb-admin-2.min.js"; //
 import "../assets2/vendor/datatables/jquery.dataTables.min.js"; //
 import "../assets2/vendor/datatables/dataTables.bootstrap4.min.js"; //
-import "../assets2/js/demo/datatables-demo.js";
+import "../assets2/js/demo/datatables-demo.js"; //
--- resources/views/components/admin-layout.blade.php
+++ resources/views/components/admin-layout.blade.php
@@ -254,5 +254,62 @@
       integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha"
       crossorigin="anonymous"
     ></script>
+
+    <script>
+      /* globals Chart:false, feather:false */
+
+(() => {
+  'use strict'
+
+  feather.replace({ 'aria-hidden': 'true' })
+
+  // Graphs
+  const ctx = document.getElementById('myChart')
+  // eslint-disable-next-line no-unused-vars
+  const myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+      ],
+      datasets: [{
+        data: [
+          15339,
+          21345,
+          18483,
+          24003,
+          23489,
+          24092,
+          12034
+        ],
+        lineTension: 0,
+        backgroundColor: 'transparent',
+        borderColor: '#007bff',
+        borderWidth: 4,
+        pointBackgroundColor: '#007bff'
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: false
+          }
+        }]
+      },
+      legend: {
+        display: false
+      }
+    }
+  })
+})()
+
+    </script>
   </body>
 </html>
